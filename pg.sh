f15aab43de755a641fccb166c2708c5c155e9a22
Niklas Karoly <niklas@karoly.io>
2021-08-28 14:13:11 +0200

    test

---
+++ pg.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+apt install curl ca-certificates gnupg -y
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+apt update
+apt install postgresql-13 -y
